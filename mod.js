ce4c8822a18ff7ebf0533ff9445472bd1d77fcd4
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2014-08-18 12:40:56 +0100

    Updated manual lists

--- mod.js
+++ mod.js
@@ -1,5 +1,5 @@
 var default_msn = "2037";
-var ezy_manuals = ["EZY-ALL-A", "EZY-ALL-A-JUN14", "EZY-A3XX-B", "EZY-A3XX-B-JUN14", "EZY-ALL-C", "EZY-ALL-CSPM", "EZY-ALL-ABS"];
+var ezy_manuals = ["EZY-ALL-A_old", "EZY-ALL-A-JUN14", "EZY-A3XX-B_old", "EZY-A3XX-B-JUN14", "EZY-ALL-C-JUN14", "EZY-ALL-CSPM", "EZY-ALL-ABS"];
 var airbus_manuals = ["EZY-A3N-FCOM", "EZY-A3N-MEL", "EZY-A3N-FCTM", "EZY-A3N-QRH"];
 
 function ezy_manualp(manual) {
