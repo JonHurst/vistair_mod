3ba4c58c133195639db73df33ba4209a3bebb414
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2014-08-27 10:51:15 +0100

    Separated out manual listing

--- ipad.js
+++ ipad.js
@@ -1,3 +1,4 @@
 document.write("<script src=\"../jquery-2.1.1.min.js\"></script>");
 document.write("<link rel='stylesheet' type=\'text/css\' href='../mod.css'/>");
+document.write("<script src=\"../manual_list.js\" charset=\"utf-8\"></script>");
 document.write("<script src=\"../mod.js\" charset=\"utf-8\"></script>");
--- mod.js
+++ mod.js
@@ -1,9 +1,4 @@
 var default_msn = "2037";
-var ezy_manuals = [
-    "EZY-ALL-A_old", "EZY-ALL-A-JUN14", "EZY-A3XX-B_old",
-    "EZY-A3XX-B-JUN14", "EZY-ALL-C-JUN14", "EZY-ALL-CSPM", "EZY-ALL-ABS"];
-var airbus_manuals = [
-    "EZY-A3N-FCOM", "EZY-A3N-MEL", "EZY-A3N-FCTM", "EZY-A3N-QRH"];
 
 function ezy_manualp(manual) {
     for(var c = 0; c < ezy_manuals.length; c++) {
